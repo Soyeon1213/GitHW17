756ddb3cf78197bff3e57554485d2e6d4bfdc1c2
In-Hye Kye <sadnody@gmail.com>
2021-04-29 19:11:29 +0900

    Add Hello world 1 in team leader

--- GitHW17/main.swift
+++ GitHW17/main.swift
@@ -2,10 +2,10 @@
 //  main.swift
 //  GitHW17
 //
-//  Created by 방문 사용자 on 2021/04/29.
+//  Created by In-Hye Kye on 2021/04/29.
 //
 
 import Foundation
 
-print("Hello, World!")
+print("Hello World 1 in team leader server")
 
